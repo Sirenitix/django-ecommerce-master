efe1d5e3e7e9f03092eccc1dbe26a77cf941de92
Sirenitix <suleevn@gmail.com>
2022-03-18 17:31:41 +0600

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -82,7 +82,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sirenitix@gmail.com'
-EMAIL_HOST_PASSWORD = 'Popidenil#1'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'From Hardware Store'
 
 
